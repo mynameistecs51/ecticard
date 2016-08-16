33011e32ca8109d5bba92de132c9b1dd8a48a747
chaiwat <mynameistecs51@gmail.com>
2016-08-16 13:03:23 +0700

    อัพเดท accommodation

---
+++ accommodation.php
@@ -0,0 +1,6 @@
+<?php include 'header.html';?>
+<div class="showpaper">
+	<img src="img/hotail.jpg" alt="" width="100%" >
+	<img src="img/hotail1.jpg" alt="" width="100%" >
+</div>
+<?php include 'footer.html';?>
