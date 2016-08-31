9b1b2d49c6f3bf30ac749cd07267ab8217db0a73
chaiwat <mynameistecs51@gmail.com>
2016-08-31 17:37:19 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	committee.php

--- committee.php
+++ committee.php
@@ -32,10 +32,10 @@
 		</tr>
 		<tr>
 			<td >
-				<div class="col-sm-6 col-md-4"></div>
+			<div class="col-sm-6 col-md-4"></div>
 				<div class="col-sm-6 col-md-4">
 					<div class="thumbnail">
-						<img src="assets/img/somsak.jpg" alt="..." style="width: 200px; height:270px; ">
+					<img src="assets/img/somsak.jpg" alt="..." style="width: 200px; height:270px; ">
 						<div class="caption text-center">
 							<p>รศ.ดร.สมศักดิ์ ชุมช่วย (สจล.)<br>นายกสมาคม ECTI </p>
 						</div>
@@ -49,183 +49,13 @@
 				<div class="col-sm-6 col-md-4"></div>
 				<div class="col-sm-6 col-md-4">
 					<div class="thumbnail">
-						<img src="assets/img/kosin.jpg" alt="..." style="width: 200px; height:270px; ">
+					<img src="assets/img/kosin.jpg" alt="..." style="width: 200px; height:270px; ">
 						<div class="caption text-center">
 							<p>ศ.ดร.โกสินทร์ จำนงไทย</p>
 						</div>
 					</div>
 				</div>
 			</td>
-		</td>
-	</tr>
-	<tr>
-		<td>	
-			<h3 class="page-header text-danger">Advisory</h3>
-			<div class="col-sm-4">		
-				<table  width="100%" class="table table-striped ">
-					<tr >
-						<td>อธิการบดี มหาวิทยาลัยราชภัฏอุดรธานี</td>
-					</tr>
-					<tr>
-						<td>อธิการบดี มหาวิทยาลัยราชภัฎเลย</td>
-					</tr>
-					<tr>
-						<td>อธิการบดี มหาวิทยาลัยราชภัฏสกลนคร</td>
-					</tr>
-					<tr colspan="3">
-						<td>ศ.ดร.ประภาส  จงสถิตวัฒนา (จุฬาลงกรณ์)</td>
-					</tr>
-					<tr>
-						<td>ศ.ดร.ประยุทธ  อัครเอกฒาสิน (มจพ)</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<tr>
-			<td >
-				<h3 class="page-header text-danger">General Co-Chair</h3>
-				<div class="col-sm-5">
-					<table width="100%" class="table table-striped ">
-						<tr >
-							<td>รศ.ดร.สมศักดิ์</td>							
-							<td>ชุ่มช่วย</td>							
-							<td>(ECTI,สจล)</td>
-						</tr>	
-						<tr >
-							<td>คุณ อัจฉรา </td>							
-							<td>เจริญสุข</td>							
-							<td> (มว.)</td>
-						</tr>	
-						<tr >
-							<td>ศ.ดร.โกสินทร์</td>							
-							<td> จานงไทย</td>							
-							<td> (มจธ.)</td>
-						</tr>	
-						<tr >
-							<td>ดร.สุทธิพงศ์</td>							
-							<td> เปรื่องค้า</td>							
-							<td> (มรภ. อุดรธานี)</td>
-						</tr>		
-						<tr >
-							<td>รศ.ดร.อลงกรณ์</td>							
-							<td> พรมที </td>							
-							<td>(มรภ. อุดรธานี)</td>
-						</tr>
-						<tr>						
-							<td>ผศ.ปรีชาศาสตร์ </td>							
-							<td> มีเกาะ </td>							
-							<td>(มรภ. สกลนคร)</td>
-						</tr>
-						<tr>						
-							<td>ดร.ชัยยศ </td>							
-							<td>คำมี </td>							
-							<td>(มรภ. เลย)</td>
-						</tr>
-					</table>
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td >
-				<h3 class="page-header text-danger">Technical Program Chairs</h3>
-				<div class="col-sm-4">
-					<table width="100%" class="table table-striped ">
-						<tr >
-							<td>รศ.ดร.อนันต์ </td>							
-							<td>ผลเพิ่ม </td>							
-							<td>(มก.)</td>
-						</tr>
-						<tr>
-							<td>ดร.ชัยวัฒน์ </td>							
-							<td>เจษฎาจินต์ </td>							
-							<td>(มว.)</td>
-						</tr>
-						<tr>
-							<td>ดร.สุชาดา</td>							
-							<td> ตันติสถิระพงษ์</td>							
-							<td> (มศว.)</td>
-						</tr>			
-					</table>
-				</div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<h3 class="page-header text-danger">Secretaries</h3>
-				<div class="col-sm-5">
-					<table width="100%" class="table table-striped ">
-						<tr >
-							<td>อ.ชัยพันธุ์ </td>
-							<td>ประการะพันธ์ </td>
-							<td>(มรภ. อุดรธานี)</td>
-						</tr>
-						<tr>
-							<td>ดร.ยุทธศักดิ์</td>
-							<td> ทอดทอง </td>
-							<td>(มรภ.อุดรธานี)	</td>
-						</tr>
-						<tr>
-							<td>คุณณัฐพงศ์ </td>
-							<td>บุตรธนู </td>
-							<td>(มรภ. อุดรธานี)</td>
-						</tr>
-						<tr>
-							<td>ผศ.ดร.กริข </td>
-							<td>สมกันธา </td>
-							<td>(มรภ. อุดรธานี)</td>
-						</tr>
-						<tr>
-							<td>ผศ.วิไลพร </td>
-							<td> กุลตังวัฒนา </td>
-							<td>(มรภ. อุดรธานี)</td>
-						</tr>
-						<tr>
-							<td>ผศ.ดร.คณิศร </td>
-							<td>จี้กระโทก </td>
-							<td>(มรภ.อุดรธานี)</td>
-						</tr>
-						<tr>
-							<td>ผศ.กิตติศักดิ์ </td>
-							<td>แสนประสิทธิ์</td>
-							<td> (มรภ.เลย)</td>
-						</tr>
-						<tr>
-							<td>ดร.คมยุทธ </td>
-							<td> ไชยวงษ์</td>
-							<td> (มรภ.เลย)</td>
-						</tr>
-						<tr>
-							<td>อ.เมืองมล </td>
-							<td>เสนเพ็ง</td>
-							<td> (มรภ.เลย)</td>
-						</tr>
-						<tr>
-							<td>อ.ภควดี </td>
-							<td>ศิริหล้า </td>
-							<td>(มรภ.เลย)</td>
-						</tr>
-						<tr>
-							<td>อ.ปริญญา</td>
-							<td> รจนา </td>
-							<td>(มรภ.สกลนคร)</td>
-						</tr>
-						<tr>
-							<td>อ.รณยุทธ </td>
-							<td>นนท์พละ</td>
-							<td> (มรภ.สกลนคร)</td>
-						</tr>
-						<tr>
-							<td>อ.ก้องภพ</td>
-							<td> ชาอามาตย์</td>
-							<td> (มรภ.สกลนคร)</td>
-						</tr>
-						<tr>
-							<td>ดร.มนตรี </td>
-							<td>โพธิโสโนทัย </td>
-							<td>(สจล.)</td>
-						</tr>
-					</table>
-				</div>
 			</td>
 		</tr>
 	</table>
