2426378919db0c40535df6fc7fa4685a59bdcf63
te chaiwat <mynameistecs51@gmail.com>
2016-08-31 07:35:16 +0700

    modified paper committee create download

--- downloadForm.php
+++ downloadForm.php
@@ -1,9 +1,9 @@
 <?php include 'header.php';?>
 <div class="row">
 
-	<p><h1>รูปแบบสำหรับส่งบทความวิจัย (Oral presentation)</h1></p>
-	<p><h1> รูปแบบสำหรับส่งสิ่งประดิษฐ์และนวัตกรรม (Poster presentation) </h1></p>
-	<p><h1>Download Template Poster</h1></p>
+	<p><h1><a href="download.php?filename=Template_research_oralpaper_ecticard2016.doc">รูปแบบสำหรับส่งบทความวิจัย (Oral presentation)</a></h1></p>
+	<p><h1><a href="download.php?filename=Template_research_posterpaper_ecticard2016.doc"> รูปแบบสำหรับส่งสิ่งประดิษฐ์และนวัตกรรม (Poster presentation)</a> </h1></p>
+	<p><h1><a href="download.php?filename=ECTI2016Template.potx"> Download Template Poster</a></h1></p>
 
 </div>
 <?php include 'footer.php';?>
--- footer.php
+++ footer.php
@@ -1,22 +1,32 @@
+	<div class="row">
+		<div class="col-sm-12 pull-right " align="right">
+			<button class="back-to-top btn btn-primary" title="Top">
+				<span class=" glyphicon glyphicon-arrow-up"><p>top</p></span>
+			</button>
+	</div>
+	</div>
 </div>
 
-      		<!-- /.col-lg-12 -->
+<!-- /.col-lg-12 -->
 
-      	</div>
+</div>
 
-      		<!-- Footer -->
-      		<footer>
-      			<div class="well">
-      				<p align="right" ">Copyright &copy; Webmaster by Mr.Chaiwat Homsang</p>
-      			</div>
-      		</footer>
-      		<!-- /.container -->
-      	</div>
-      	<script type="text/javascript">
-                  $(function(){
-                        $('.dropdown-toggle').dropdownHover({delay:0});
-                  });
-            </script>
-      </body>
+<!-- Footer -->
+<footer>
+	<div class="well">
+		<p align="right" ">Copyright &copy; Webmaster by Mr.Chaiwat Homsang</p>
+	</div>
+</footer>
+<!-- /.container -->
+</div>
+<script type="text/javascript">
+	$(function(){
+		$('.dropdown-toggle').dropdownHover({delay:0});	//menuhover
+		$('.back-to-top').click(function(){	//buttom back to top page
+			$('html,body').animate({scrollTop:0},'slow');return false;
+		});
+	});
+</script>
+</body>
 
-      </html>
+</html>
