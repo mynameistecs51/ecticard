182f9023679904d33823ff3fd76313f370b632eb
chaiwat <mynameistecs51@gmail.com>
2016-09-16 09:47:52 +0700

    update submit paper  for bank page base system cjass

--- regis.php
+++ regis.php
@@ -1,7 +1,33 @@
 <?php include "header.php";?>
-<h1>
-	พิเศษ!!! สำหรับผู้ลงทะเบียนประเภท Regular ในช่วง
-	Early Bird รับสิทธิ์เป็น ECTI Member ฟรี 1 ปี!!!
-</h1>
+<p>
+	<h3>
+		กำหนดชำระเงิน Early Bird ภายในวันที่ 24 มิถุนายน 2560 เพื่อตีพิมพ์ลง Proceeding  <b style="color:blue;"><a class="text-danger" href="http://www.confserve.org/cjass/login.php?event_id=CJASS" target="bank"><u>ชำระเงิน</u></a></b> โดยสามารถศึกษาวิธีการตาม <b style="color:blue;">เอกสารคู่มือ</b>
+	</h3>
+</p>
+<p>
+	<h3>
+		Download <b style="color:blue;"> กำหนดการนำเสนอผลงาน </b> <br>
+		*ผู้นำเสนอบทความทุกท่าน ต้องส่งไฟล์นำเสนอ (PPT และ PDF) ณ ห้องนำเสนอ ก่อนเริ่ม Session 15 นาที
+	</h3>
+</p>
+<p>
+	<h3>
+		Download <b style="color:blue;">จดหมายตอบรับบทความ</b>
+	</h3>
+</p>
+<p>
+<h2 >
+	<a class="text-danger" href="http://www.confserve.org/cjass/login.php?event_id=CJASS" target="bank"><u>:: คลิกเพื่อ ลงทะเบียน ::</u></a>
+</h2>
+	<h3>
+		พิเศษ!!! สำหรับผู้ลงทะเบียนประเภท Regular ในช่วง
+		Early Bird รับสิทธิ์เป็น ECTI Member ฟรี 1 ปี!!!
+	</h3>
 	<img src="assets/img/registration.jpg" alt="" class="col-sm-12" width="100%">
+</p>
+<p >
+	<h3 style="color:green;">
+		แจ้งปัญหาการลงทะเบียนหรือ Payment  กรุณาติดต่อ หรือส่งหลักฐานได้ทาง <br>  Email: cjass.help@gmail.com ครับ
+	</h3>
+</p>
 <?php include "footer.php";?>
