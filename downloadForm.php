a7c78d7645d64f70c85cd1c6a20499b060a412a0
chaiwat <mynameistecs51@gmail.com>
2016-11-20 18:47:31 +0700

    update download paper oral template

--- downloadForm.php
+++ downloadForm.php
@@ -1,8 +1,8 @@
 <?php include 'header.php';?>
 <div class="row">
 
-	<p><h1><a href="download.php?filename=Template_research_oralpaper_ecticard2016.doc">รูปแบบสำหรับส่งบทความวิจัย (Oral presentation)</a></h1></p>
-	<p><h1><a href="download.php?filename=Template_research_posterpaper_ecticard2016.doc"> รูปแบบสำหรับส่งสิ่งประดิษฐ์และนวัตกรรม (Poster presentation)</a> </h1></p>
+	<p><h1><a href="download.php?filename=Template_research_oralpaper_ecticard2017.doc">รูปแบบสำหรับส่งบทความวิจัย (Oral presentation)</a></h1></p>
+	<p><h1><a href="download.php?filename=Template_research_poster_ecticard2017.doc"> รูปแบบสำหรับส่งสิ่งประดิษฐ์และนวัตกรรม (Poster presentation)</a> </h1></p>
 	<p><h1><a href="download.php?filename=ECTI2016Template.potx"> Download Template Poster</a></h1></p>
 
 </div>
