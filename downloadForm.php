3f33d6d514dcfd3627421ac69a42f5f126818a23
chaiwat <mynameistecs51@gmail.com>
2016-11-30 11:45:20 +0700

    update filename

--- downloadForm.php
+++ downloadForm.php
@@ -3,7 +3,7 @@
 
 	<p><h1><a href="download.php?filename=Template_research_oralpaper_ecticard2017.doc">รูปแบบสำหรับส่งบทความวิจัย (Oral presentation)</a></h1></p>
 	<p><h1><a href="download.php?filename=Template_research_poster_ecticard2017.doc"> รูปแบบสำหรับส่งสิ่งประดิษฐ์และนวัตกรรม (Poster presentation)</a> </h1></p>
-	<p><h1><a href="download.php?filename=ECTI2016Template.potx"> Download Template Poster</a></h1></p>
+	<p><h1><a href="download.php?filename=ECTI2017Template.potx"> Download Template Poster</a></h1></p>
 
 </div>
 <?php include 'footer.php';?>
