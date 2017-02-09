4424f13c6e8301d191a5108eeb2ac22d9e34fc50
chaiwat <mynameistecs51@gmail.com>
2017-02-09 13:58:00 +0700

    update committee

--- committee.php
+++ committee.php
@@ -43,53 +43,58 @@
 				</div>
 			</td>
 		</tr>
-		<tr>
+		<!-- <tr>
 			<td>
 				<h3 class="page-header text-danger">General Co-Chair</h3>
 				<div class="col-sm-6 col-md-4"></div>
 				<div class="col-sm-6 col-md-4">
 					<div class="thumbnail">
-						<!-- <img src="assets/img/kosin.jpg" alt="..." style="width: 200px; height:270px; "> -->
+						<img src="assets/img/kosin.jpg" alt="..." style="width: 200px; height:270px; ">
 						<div class="caption text-center">
 							<p>ศ.ดร.โกสินทร์ จำนงไทย</p>
 						</div>
 					</div>
 				</div>
 			</td>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<h3 class="page-header text-danger">Advisory</h3>
-			<div class="col-sm-4">
-				<table  width="100%" class="table table-striped ">
-					<tr >
-						<td>อธิการบดี มหาวิทยาลัยราชภัฏอุดรธานี</td>
-					</tr>
-					<tr>
-						<td>อธิการบดี มหาวิทยาลัยราชภัฎเลย</td>
-					</tr>
-					<tr>
-						<td>อธิการบดี มหาวิทยาลัยราชภัฏสกลนคร</td>
-					</tr>
-					<tr colspan="3">
-						<td>ศ.ดร.ประภาส  จงสถิตวัฒนา (จุฬาลงกรณ์)</td>
-					</tr>
-					<tr>
-						<td>ศ.ดร.ประยุทธ  อัครเอกฒาสิน (มจพ.)</td>
-					</tr>
-				</table>
+		</tr> -->
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Advisory</h3>
+				<div class="col-sm-6">
+					<table  width="100%" class="table table-striped ">
+						<tr >
+							<td>อธิการบดี มหาวิทยาลัยราชภัฏอุดรธานี</td>
+						</tr>
+						<tr>
+							<td>อธิการบดี มหาวิทยาลัยราชภัฎเลย</td>
+						</tr>
+						<tr>
+							<td>อธิการบดี มหาวิทยาลัยราชภัฏสกลนคร</td>
+						</tr>
+						<tr colspan="3">
+							<td>ศ.ดร.ประภาส  จงสถิตวัฒนา (จุฬาลงกรณ์)</td>
+						</tr>
+						<tr>
+							<td>ศ.ดร.ประยุทธ  อัครเอกฒาสิน (มจพ.)</td>
+						</tr>
+					</table>
+				</div>
 			</td>
 		</tr>
-		<tr>
+		<tr >
 			<td >
 				<h3 class="page-header text-danger">General Co-Chair</h3>
-				<div class="col-sm-5">
+				<div class="col-sm-6">
 					<table width="100%" class="table table-striped ">
 						<tr >
 							<td>รศ.ดร.สมศักดิ์</td>
 							<td>ชุ่มช่วย</td>
 							<td>(ECTI,สจล.)</td>
+						</tr>
+						<tr>
+							<td> ดร.เกียรติศักดิ์ </td>
+							<td>ศรีพิมานวัฒน์ </td>
+							<td>(ECTI)</td>
 						</tr>
 						<tr >
 							<td>คุณ อัจฉรา </td>
@@ -121,6 +126,16 @@
 							<td>คำมี </td>
 							<td>(มรภ. เลย)</td>
 						</tr>
+						<tr>
+							<td>ดร.ศักดิ์ดา </td>
+							<td>พรรณไวย </td>
+							<td>(กทพ.)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ดร.กฤษฎา </td>
+							<td>ณ หนองคาย</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
 					</table>
 				</div>
 			</td>
@@ -128,7 +143,7 @@
 		<tr>
 			<td >
 				<h3 class="page-header text-danger">Technical Program Chairs</h3>
-				<div class="col-sm-4">
+				<div class="col-sm-6">
 					<table width="100%" class="table table-striped ">
 						<tr >
 							<td>รศ.ดร.อนันต์ </td>
@@ -145,27 +160,52 @@
 							<td> ตันติสถิระพงษ์</td>
 							<td> (มศว.)</td>
 						</tr>
+						<tr>
+							<td  >ผศ.ดร.ณัฐพงศ์ </td>
+							<td> พันธุนะ </td>
+							<td>(มทร. พระนคร)</td>
+						</tr>
 					</table>
 				</div>
 			</td>
 		</tr>
 		<tr>
 			<td>
-				<h3 class="page-header text-danger">Secretaries</h3>
-				<div class="col-sm-5">
+				<h3 class="page-header text-danger">Committee</h3>
+				<div class="col-sm-6">
 					<table width="100%" class="table table-striped ">
-						<tr >
-							<td>อ.ชัยพันธุ์ </td>
-							<td>ประการะพันธ์ </td>
+						<tr>
+							<td>ดร.มนตรี </td>
+							<td> โพธิโสโนทัย </td>
+							<td>(สจล.)</td>
+						</tr>
+						<tr>
+							<td>ดร. สถาพร </td>
+							<td>พรพรหมลิขิต </td>
+							<td>(มข.)</td>
+						</tr>
+						<tr>
+							<td>รศ.ดร. อภินันท์ </td>
+							<td>อุรโสภณ </td>
+							<td>(มมส.)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ปองพล </td>
+							<td>แสนสอน </td>
 							<td>(มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
-							<td>ดร.ยุทธศักดิ์</td>
-							<td> ทอดทอง </td>
-							<td>(มรภ.อุดรธานี)	</td>
+							<td>ดร.ยุทธศักดิ์ </td>
+							<td>ทอดทอง </td>
+							<td>(มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
-							<td>คุณณัฐพงศ์ </td>
+							<td>สมชาย </td>
+							<td>สิริพัฒนากุล </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ณัฐพงศ์ </td>
 							<td>บุตรธนู </td>
 							<td>(มรภ. อุดรธานี)</td>
 						</tr>
@@ -176,58 +216,486 @@
 						</tr>
 						<tr>
 							<td>ผศ.วิไลพร </td>
-							<td> กุลตังวัฒนา </td>
+							<td>กุลตังวัฒนา </td>
 							<td>(มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
 							<td>ผศ.ดร.คณิศร </td>
 							<td>จี้กระโทก </td>
-							<td>(มรภ.อุดรธานี)</td>
+							<td>(มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
-							<td>นายไชยวัฒน์   </td>
-							<td>หอมแสง </td>
-							<td>(มรภ.อุดรธานี)</td>
+							<td>ผศ. ดร. สุนทร </td>
+							<td>สุทธิบาก </td>
+							<td>(มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
-							<td>ผศ.กิตติศักดิ์ </td>
-							<td>แสนประสิทธิ์</td>
-							<td> (มรภ.เลย)</td>
+							<td>ผศ. ดร. วราจิต </td>
+							<td>พยอม </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ผศ สุทิน </td>
+							<td>พลบูรณ์ </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ณัฐกิตติ์ </td>
+							<td>แสนทอง </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>วิภา </td>
+							<td>เหลืองบุตรนาค </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>รณฤทธิ์ </td>
+							<td>จันทร์ศิริ </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>สรวินท์ </td>
+							<td>ปูคะภาค </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ดร. เอกราช </td>
+							<td> ดีนาง</td>
+							<td>มรภ. อุดรธานี)</td>
 						</tr>
 						<tr>
 							<td>ดร.คมยุทธ </td>
-							<td> ไชยวงษ์</td>
-							<td> (มรภ.เลย)</td>
+							<td>ไชยวงษ์</td>
+							<td>(มรภ. เลย)</td>
 						</tr>
 						<tr>
-							<td>อ.เมืองมล </td>
-							<td>เสนเพ็ง</td>
-							<td> (มรภ.เลย)</td>
+							<td>ผศ.ศักดิ์ชาย</td>
+							<td> พวงจันทร์</td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ยุทธพงษ์ </td>
+							<td>นาคโสภณ</td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.เมืองมล</td>
+							<td> เสนเพ็ง </td>
+							<td>(มรภ. เลย)</td>
 						</tr>
 						<tr>
 							<td>อ.ภควดี </td>
 							<td>ศิริหล้า </td>
-							<td>(มรภ.เลย)</td>
+							<td>(มรภ. เลย)</td>
 						</tr>
 						<tr>
-							<td>อ.ปริญญา</td>
-							<td> รจนา </td>
-							<td>(มรภ.สกลนคร)</td>
+							<td>อ.ปกรณ์เกียรติ </td>
+							<td>ภูกองพลอย</td>
+							<td>(มรภ. เลย)</td>
 						</tr>
 						<tr>
-							<td>อ.รณยุทธ </td>
-							<td>นนท์พละ</td>
-							<td> (มรภ.สกลนคร)</td>
+							<td>อ.วันชาติ</td>
+							<td> สุพรมพิทักษ์ </td>
+							<td>(มรภ. เลย)</td>
 						</tr>
 						<tr>
-							<td>อ.ก้องภพ</td>
-							<td> ชาอามาตย์</td>
-							<td> (มรภ.สกลนคร)</td>
+							<td>อ.เปรมชัย </td>
+							<td>มูลหล้า</td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.ณัฐชัย</td>
+							<td> โปธิ </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ผศ. ดร. สราวุฒิ</td>
+							<td> บุญเกิดรัมย์ </td>
+							<td>(มรภ. สกลนคร)</td>
+						</tr>
+						<tr>
+							<td>ผศ. รชต </td>
+							<td>บุณยะยุต </td>
+							<td>(มรภ. สกลนคร)</td>
+						</tr>
+						<tr>
+							<td>รณยุทธ </td>
+							<td>นนท์พละ </td>
+							<td>(มรภ. สกลนคร)</td>
+						</tr>
+						<tr>
+							<td>ยิ่งยศ </td>
+							<td>ศรีบุญเรือง </td>
+							<td>(มบ. สระแก้ว)</td>
+						</tr>
+						<tr>
+							<td>ดร. นิกร </td>
+							<td>แก้วแพรก</td>
+							<td>(วว.)</td>
+						</tr>
+						<tr>
+							<td>ดร. สัญญา</td>
+							<td> ควรคิด </td>
+							<td>(มรภ. นครปฐม)</td>
+						</tr>
+						<tr>
+							<td>ดร. ธวัชชัย </td>
+							<td>ทองเหลี่ยม</td>
+							<td> (มรภ. นครปฐม)</td>
+						</tr>
+						<tr>
+							<td>ผศ.พนม </td>
+							<td>ท้าวดี</td>
+							<td> (มก. สกลนคร)</td>
+						</tr>
+						<tr>
+							<td>ธนาฒน์ </td>
+							<td>สุกนวล </td>
+							<td>(มทร. อีสาน-นครราชสีมา)</td>
+						</tr>
+						<tr>
+							<td>ผศ.วิชชุพงษ์</td>
+							<td> วิบูลเจริญ </td>
+							<td>(มทร. อีสาน-นครราชสีมา)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ดร.อดิเรก</td>
+							<td> จันตะคุณ</td>
+							<td> (มทร. อีสาน-ขอนแก่น)</td>
+						</tr>
+						<tr>
+							<td>ศราวุฑ</td>
+							<td> คงลำพันธ์ </td>
+							<td>(มทร. อีสาน-ขอนแก่น)</td>
+						</tr>
+						<tr>
+							<td>กร</td>
+							<td> พวงนาค </td>
+							<td>(มทร. พระนคร)</td>
+						</tr>
+						<tr>
+							<td>มัณฑณา</td>
+							<td> เตี๋ยวงษ์สุวรรณ์ </td>
+							<td>(มทร. พระนคร)</td>
+						</tr>
+						<tr>
+							<td>ผศ. จิรพงษ์</td>
+							<td> จิตตะโคตร์ </td>
+							<td>(มทร. ธัญบุรี)</td>
+						</tr>
+						<tr>
+							<td>ดร. สายชล </td>
+							<td>ชุดเจือจีน </td>
+							<td>(มทร. กรุงเทพ)</td>
+						</tr>
+
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Local Arrangement Chairs</h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ผศ.กิตติศักดิ์ </td>
+							<td>แสนประสิทธ์ </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ดร.คมยุทธ </td>
+							<td>ไชยวงษ์</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.เมืองมล </td>
+							<td>เสนเพ็ง </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.ภควดี </td>
+							<td>ศิริหล้า </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.พัฒนะ </td>
+							<td>เจริญยิ่ง</td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.ณัชชา</td>
+							<td> สมจันทร์ </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.เปรมชัย</td>
+							<td> มูลหล้า </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>อ.ศุภกัญญา </td>
+							<td>ขันชัยภูมิ </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>นางทัศนีย์ </td>
+							<td>บุตรเพ็ง</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>นายไพโรจน์ </td>
+							<td>บุตรเพ็ง </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>นายสุรชัย </td>
+							<td>กองชนิน</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ชัยพันธุ์ </td>
+							<td>ประการะพันธ์ </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Information Management Chairs</h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ผศ.ดร.กริข </td>
+							<td>สมกันธา </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ไชยวัฒน์ </td>
+							<td>หอมแสง</td>
+							<td> (มรภ.อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ผศ.วิไลพร </td>
+							<td>กุลตังวัฒนา</td>
+							<td> (มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ดร.คณิศร </td>
+							<td>จี้กระโทก </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ชัยพันธุ์ </td>
+							<td>ประการะพันธ์</td>
+							<td> (มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>กร </td>
+							<td>พวงนาค </td>
+							<td>(มทร. พระนคร)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Publication Chairs</h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ผศ.ดร. ลือชัย </td>
+							<td>พรหมรัตน์รักษ์</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ผศ. จุไรรัตน์จินดา</td>
+							<td> อรรคนิตย์</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ดร.ภูเบศร์ </td>
+							<td>พิพิธหิรัญการ</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>วิภา</td>
+							<td> เหลืองบุตรนาค</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>กาญจนา </td>
+							<td>มูลอา</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>รศ.เกียรติศักดิ์ </td>
+							<td>รัตนสีหา </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ดร.ภาณุมาศ </td>
+							<td>พรหมเทศ</td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ผศ.ดร.สุรจิตร์ </td>
+							<td>พระเมือง</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ผศ.สมศักดิ์ </td>
+							<td>พินิจด่านกลาง</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Workshop &amp; Exhibition Chairs </h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ผศ.ปองพล </td>
+							<td>แสนสอน </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ดร.ยุทธศักดิ์ </td>
+							<td>ทอดทอง </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>สมชาย </td>
+							<td>สิริพัฒนากุล </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ณัฐพงศ์ </td>
+							<td>บุตรธนู </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Finance &amp; Registration Chairs </h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>วิบูลย์ </td>
+							<td>พร้อมนิชย์ </td>
+							<td>(สจล.)</td>
+						</tr>
+						<tr>
+
+							<td>บัววร </td>
+							<td>ชัยธงรัตน์</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+
+							<td>ชัยพันธุ์ </td>
+							<td>ประการะพันธ์</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+
+							<td>ไพริน</td>
+							<td> แก้วกรวย</td>
+							<td>(ECTI)</td>
+						</tr>
+						<tr>
+
+							<td>หัถยา </td>
+							<td>คำภาษี</td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+
+							<td>อ.ศุภกัญญา</td>
+							<td> ขันชัยภูมิ</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+
+							<td>นางทัศนีย์ </td>
+							<td>บุตรเพ็ง </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+
+							<td>นางชฎาพร </td>
+							<td>คุชนุช </td>
+							<td>(มรภ. เลย)</td>
+						</tr>
+						<tr>
+
+							<td>นางสาวสุภัทตรา </td>
+							<td>พันธุวาปี</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+
+							<td>นางสาวภัทราภรณ์</td>
+							<td> เขียวทองสี</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Secretaries </h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ชัยพันธุ์</td>
+							<td> ประการะพันธ์ </td>
+							<td>(มรภ. อุดรธานี)</td>
+						</tr>
+						<tr>
+							<td>ผศ.กิตติศักดิ์ </td>
+							<td>แสนประสิทธ์</td>
+							<td> (มรภ. เลย)</td>
+						</tr>
+						<tr>
+							<td>ปริญญา </td>
+							<td>รจนา</td>
+							<td> (มรภ. สกลนคร)</td>
+						</tr>
+						<tr>
+							<td>ไมตรี</td>
+							<td> ธรรมมา</td>
+							<td> (มทร. อีสาน)</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3 class="page-header text-danger">Special Session </h3>
+				<div class="col-sm-6">
+					<table width="100%" class="table table-striped ">
+						<tr>
+							<td>ผศ.ดร.เกริก </td>
+							<td>ภิรมย์โสภา</td>
+							<td> (จุฬาลงกรณ์)</td>
 						</tr>
 						<tr>
 							<td>ดร.มนตรี </td>
-							<td>โพธิโสโนทัย </td>
-							<td>(สจล.)</td>
+							<td>โพธิโสโนทัย</td>
+							<td> (สจล.)</td>
 						</tr>
 					</table>
 				</div>
@@ -235,4 +703,5 @@
 		</tr>
 	</table>
 </div>
+
 <?php include "footer.php";?>
