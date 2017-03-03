06d91303f8a41ca51284caceff15fe377b4174c9
chaiwat <mynameistecs51@gmail.com>
2017-03-03 22:19:10 +0700

    update permission

--- keynotespeaker.php
+++ keynotespeaker.php
@@ -1,10 +1,31 @@
 <?php include 'header.php';?>
+<style type="text/css">
+	p , li {
+		text-align: justify;
+		font-size: 14pt;
+	}
+	td{
+		text-align: left;
+	}
+</style>
 <div class="row">
 	<div class="col-sm-12 text-center">
 		<img src="assets/img/Pansak_siri.png" class="img-rounded" width="200" height="236">
 		<div class="content">
-			<p style="text-align: justify; font-size: 16pt;">
-				<b>ดร. พันธ์ศักดิ์ ศิริรัชตพงษ์</b> ได้รับปริญญา วิศวกรรมศาสตรบัณฑิต (ไฟฟ้า) ในปี ๒๕๒๐  มหาวิทยาลัยเกษตรศาสตร์
+			<p >
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>ดร. พันธ์ศักดิ์ ศิริรัชตพงษ์</b> ได้รับปริญญา วิศวกรรมศาสตรบัณฑิต (ไฟฟ้า) ในปี ๒๕๒๐  ที่มหาวิทยาลัยเกษตรศาสตร์, วิศวกรรมศาสตรมหาบัณฑิต (อิเล็กทรอนิกส์) ในปี ๒๕๒๒ ที่มหาวิทยาลัยเกษตรศาสตร์,  DEA. (Electronics) ในปี ๒๕๒๔ ที่ Institute National Polytechnique de Grenoble (INPG), สาธารณรัฐฝรั่งเศส, Diplôme d'ingénieur (D.ING) ในปี ๒๕๒๗ ที่ Institute National Polytechnique de Grenoble (INPG), สาธารณรัฐฝรั่งเศส
+				<br>
+				<u>ตำแหน่งหน้าที่คัญในอดีต</u>
+				<table class="col-sm-12" border="1">
+					<tr>
+						<td class="col-sm-3 ">- ปัจจุบัน</td>
+						<td class=" col-sm-9 ">ผู้ช่วยรัฐมนตรีประจำกระทรวงดิจิทัลเพื่อเศรษฐกิจและสังคม</td>
+					</tr>
+					<tr>
+						<td class="col-sm-3 ">- พ.ศ. ๒๕๕๗ - พ.ศ. ๒๕๕๘</td>
+						<td class=" col-sm-9 ">ผุ้ช่วยรัฐมนตรีประจำกระทรวงเทคโนโลยีสารสนเทศและการสื่อสาร</td>
+					</tr>
+				</table>
 			</p>
 		</div>
 	</div>
