159334ca80c303683220396f86f541252360b20a
chaiwat <mynameistecs51@gmail.com>
2017-03-03 21:42:07 +0700

    update keynotepeaker

--- keynotespeaker.php
+++ keynotespeaker.php
@@ -1,3 +1,12 @@
 <?php include 'header.php';?>
-<h1>รออัพเดทข้อมูล</h1>
+<div class="row">
+	<div class="col-sm-12 text-center">
+		<img src="assets/img/Pansak_siri.png" class="img-rounded" width="200" height="236">
+		<div class="content">
+			<p style="text-align: justify; font-size: 16pt;">
+				<b>ดร. พันธ์ศักดิ์ ศิริรัชตพงษ์</b> ได้รับปริญญา วิศวกรรมศาสตรบัณฑิต (ไฟฟ้า) ในปี ๒๕๒๐  มหาวิทยาลัยเกษตรศาสตร์
+			</p>
+		</div>
+	</div>
+</div>
 <?php include 'footer.php'; ?>
