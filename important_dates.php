b7cdc064241cbf4f4ad773e49a52e30f3f63cc24
chaiwat <mynameistecs51@gmail.com>
2017-05-15 14:58:57 +0700

    update file

--- important_dates.php
+++ important_dates.php
@@ -3,8 +3,8 @@
 	<div class="panel panel-default">
 	<div class="panel-heading"><i class="fa fa-calendar" aria-hidden="true"></i> IMPORTANT DATES</div>
 		<div class="panel-body">
-			<p ><h3 class="text-danger col-sm-10"> - Paper Submission Deadline For Review  31 มีนาคม 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </p>
-			<p><h3 class="col-sm-10"> - Decision Notification 11 พฤษภาคม 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </p>
+			<p ><strike><h3 class="text-danger col-sm-10"> - Paper Submission Deadline For Review  31 มีนาคม 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </strike></p>
+			<p><h3 class="col-sm-10"> - Decision Notification 26 พฤษภาคม 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </p>
 			<p><h3 class="text-info col-sm-10"> - Camera Ready Version 1 มิถุนายน 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </p>
 			<p><h3 class="text-success col-sm-10"> - Conference day 25 - 28 กรกฏาคม 2560 <img src="assets/img/new_icon.gif" alt="" class="col-sm-1 "></h3> </p>
 		</div>
