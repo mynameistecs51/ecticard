5f477a5d908767537c1c72ae8235f7c53da006eb
chaiwat <mynameistecs51@gmail.com>
2017-05-29 08:31:37 +0700

    update sponsor

--- sponsor.php
+++ sponsor.php
@@ -60,34 +60,44 @@
 </div> -->
 <div class="row">
 	<div class="col-xs-6 col-md-3">
-		<a href="#" class="thumbnail">
+		<a class="lightbox thumbnail" href="assets/img/ecti-banner.PNG" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/ecti-banner.PNG" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-		<a href="#" class="thumbnail">
+			<a class="lightbox thumbnail" href="assets/img/Road.png" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/Road.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-		<a href="#" class="thumbnail">
+			<a class="lightbox thumbnail" href="assets/img/BannerNIMT.gif" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/BannerNIMT.gif" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-		<a href="#" class="thumbnail">
-		<img src="assets/img/LOGO_GIC.png" alt="..." style="width:250px;height: 140px;">
+			<a class="lightbox thumbnail" href="assets/img/LOGO_GIC.png" data-littlelightbox-group="gallery" title="" >
+			<img src="assets/img/LOGO_GIC.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-		<a href="#" class="thumbnail">
-		<img src="assets/img/logo_marigold.png" alt="..." style="width:250px;height: 140px;">
+			<a class="lightbox thumbnail" href="assets/img/logo_marigold.png" data-littlelightbox-group="gallery" title="" >
+			<img src="assets/img/logo_marigold.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-		<a href="#" class="thumbnail">
-		<img src="assets/img/logo_aswell.jpg" alt="..." style="width:250px;height: 140px;">
+			<a class="lightbox thumbnail" href="assets/img/logo_aswell.jpg" data-littlelightbox-group="gallery" title="" >
+			<img src="assets/img/logo_aswell.jpg" alt="..." style="width:250px;height: 140px;">
+		</a>
+	</div>
+	<div class="col-xs-6 col-md-3 col-sm-3">
+			<a class="lightbox thumbnail" href="assets/img/LOGO_TRANSCODE.jpg" data-littlelightbox-group="gallery" title="" >
+			<img src="assets/img/LOGO_TRANSCODE.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 </div>
+
+
+<script>
+	$('.lightbox').littleLightBox();
+</script>
 <?php include 'footer.php';?>
