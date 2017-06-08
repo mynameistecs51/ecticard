7c5f671470f36736f544030f4ec621db63941d6f
chaiwat <mynameistecs51@gmail.com>
2017-06-08 14:56:03 +0700

    update base paper aword

--- basetPaperAward.php
+++ basetPaperAward.php
@@ -1,3 +1,6 @@
 <?php include 'header.php';?>
-<h1>รออัพเดทข้อมูล</h1>
+<div class="row ">
+	<!-- <p align="center"> .::  Best Paper Award from EXAT ::.</p> -->
+	<img src="assets/img/basePaperAwardformExtat.png" alt="" class="col-sm-12" style="width:100%">
+</div>
 <?php include 'footer.php'; ?>
