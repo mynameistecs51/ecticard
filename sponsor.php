f5b2dfbc5c0024840da8b5d4ad15946b91a5b33e
chaiwat <mynameistecs51@gmail.com>
2017-06-20 09:52:58 +0700

    update sponsor code merge

--- sponsor.php
+++ sponsor.php
@@ -1,63 +1,4 @@
 <?php include 'header.php';?>
-<!-- <div class="row">
-	<div class="col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<img src="assets/img/udru.gif" alt="..." style="width: 200px;">
-			<div class="caption text-center">
-				<h3>มหาวิทยาลัยราชภัฏอุดรธานี</h3>
-			</div>
-		</div>
-	</div>
-	<div class="col-sm-8" style="text-indent:1cm;" align="justify">
-		มหาวิทยาลัยราชภัฏอุดรธานี เดิมชื่อว่า “โรงเรียนฝึกหัดครูกสิกรรมมณฑลอุดร” ก่อตั้งเมื่อวันที่ 1 พฤศจิกายน พ.ศ. 2466 มีราชบุรุษเพิ่ม การสมศีล ทำหน้าที่แทนครูใหญ่ เปิดสอนหลักสูตรครูประกาศนียบัตรมณฑล โดยรับนักเรียนชายที่จบประถมศึกษาปีที่ 4 เข้าศึกษาต่ออีก 2 ปี นักเรียนเหล่านี้เป็นนักเรียนทุนจากจังหวัดต่าง ๆ ในมณฑล
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		วันที่ 10 มิถุนายน พ.ศ. 2547 พระบาทสมเด็จพระเจ้าอยู่หัวภูมิพลอดุลยเดช ทรงลงพระปรมาภิไธย พระราชบัญญัติมหาวิทยาลัยราชภัฏ พ.ศ. 2547 และประกาศในราชกิจจานุเบกษา เมื่อวันที่ 14 มิถุนายน พ.ศ. 2547 ส่งผลให้สถาบันราชภัฏอุดรธานี ได้รับการยกฐานะและปรับเปลี่ยนสถานภาพเป็น “มหาวิทยาลัยราชภัฏอุดรธานี” ตั้งแต่วันที่ 15 มิถุนายน พ.ศ. 2547 เป็นต้นมา
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		สถานที่ตั้งมหาวิทยาลัยราชภัฏอุดรธานี
-		64 มหาวิทยาลัยราชภัฏอุดรธานี ถนนทหาร ตำบลหมากแข้ง
-		อำเภอเมือง จังหวัดอุดรธานี 41000
-	</div>
-</div>
-<hr>
-<div class="row">
-	<div class="col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<img src="assets/img/sakon.gif" alt="..." style="width: 200px;">
-			<div class="caption text-center">
-				<h3>มหาวิทยาลัยราชภัฏสกลนคร</h3>
-			</div>
-		</div>
-	</div>
-	<div class="col-sm-8" style="text-indent:1cm;" align="justify">
-		มหาวิทยาลัยราชภัฏสกลนคร เป็นมหาวิทยาลัยของรัฐ ตั้งอยู่ในอำเภอเมืองสกลนคร จังหวัดสกลนคร ก่อตั้งขึ้นเมื่อปี พ.ศ. 2507 ในชื่อ "โรงเรียนฝึกหัดครูสกลนคร" (วิกิพีเดีย)
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		ที่อยู่: 680, Nittayo Road, Tambon That Choeng Chum, Amphoe Mueang Sakon Nakhon,, Sakon Nakhon, 47000
-		ผู้ก่อตั้ง: กระทรวงศึกษาธิการ (ประเทศไทย)
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		ก่อตั้ง: 17 กรกฎาคม 2507
-		โทรศัพท์: 042 970 021
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		สี: สีเขียว, สีชมพู
-	</div>
-</div>
-<hr>
-<div class="row">
-	<div class="col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<img src="assets/img/lru.gif" alt="..." style="width: 400px;">
-			<div class="caption text-center">
-				<h3>มหาวิทยาลัยราชภัฏเลย</h3>
-			</div>
-		</div>
-	</div>
-	<div class="col-sm-8" style="text-indent:1cm;" align="justify">
-		มหาวิทยาลัยราชภัฏเลย เป็นสถาบันอุดมศึกษา ก่อตั้งขึ้นเมื่อปี พ.ศ. 2516 เดิมมีฐานะเป็น "วิทยาลัยครูเลย" เป็นสถาบันการศึกษาระดับอุดมศึกษาในสังกัดกระทรวงศึกษาธิการ ได้รับการสถาปนาเมื่อวันที่ 29 กันยายน พ.ศ. 2516 (วิกิพีเดีย)
-		<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		ที่อยู่: 234 ถนน, Soi Loei-chiang Khan 1, Mueang, Mueang Loei District, Loei 42000
-		ก่อตั้ง: 29 กันยายน 2516
-		โทรศัพท์: 042 813 060
-	</div>
-</div> -->
 <div class="row">
 	<div class="col-xs-6 col-md-3">
 		<a class="lightbox thumbnail" href="assets/img/ecti-banner.PNG" data-littlelightbox-group="gallery" title="" >
@@ -65,47 +6,47 @@
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-			<a class="lightbox thumbnail" href="assets/img/Road.png" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/Road.png" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/Road.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-			<a class="lightbox thumbnail" href="assets/img/BannerNIMT.gif" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/BannerNIMT.gif" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/BannerNIMT.gif" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3">
-			<a class="lightbox thumbnail" href="assets/img/LOGO_GIC.png" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/LOGO_GIC.png" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/LOGO_GIC.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/logo_marigold.png" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/logo_marigold.png" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_marigold.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/logo_aswell.jpg" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/logo_aswell.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_aswell.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/LOGO_TRANSCODE.jpg" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/LOGO_TRANSCODE.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/LOGO_TRANSCODE.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/logo_ThaiEasyElec_education.png" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/logo_ThaiEasyElec_education.png" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_ThaiEasyElec_education.png" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/logo_loxley.jpg" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/logo_loxley.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_loxley.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-			<a class="lightbox thumbnail" href="assets/img/logo_BEM.jpg" data-littlelightbox-group="gallery" title="" >
+		<a class="lightbox thumbnail" href="assets/img/logo_BEM.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_BEM.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
 	</div>
@@ -115,7 +56,6 @@
 		</a>
 	</div>
 	<div class="col-xs-6 col-md-3 col-sm-3">
-<<<<<<< HEAD
 		<a class="lightbox thumbnail" href="assets/img/logo_iceLed.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_iceLed.jpg" alt="..." style="width:250px;height: 140px;">
 		</a>
@@ -138,14 +78,14 @@
 	<div class="col-xs-6 col-md-3 col-sm-3">
 		<a class="lightbox thumbnail" href="assets/img/logo_ITS.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_ITS.jpg" alt="..." style="width:250px;height: 140px;">
-=======
+		</a>
+	</div>
+	<div class="col-xs-6 col-md-3 col-sm-3">
 		<a class="lightbox thumbnail" href="assets/img/logo_Forth.jpg" data-littlelightbox-group="gallery" title="" >
 			<img src="assets/img/logo_Forth.jpg" alt="..." style="width:250px;height: 140px;">
->>>>>>> origin/master
 		</a>
 	</div>
 </div>
-
 
 <script>
 	$('.lightbox').littleLightBox();
