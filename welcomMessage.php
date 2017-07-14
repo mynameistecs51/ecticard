96354aec83c75734da76bbc95d38a979653e6588
chaiwat <mynameistecs51@gmail.com>
2017-07-14 10:11:23 +0700

    update program ad a glance

--- welcomMessage.php
+++ welcomMessage.php
@@ -2,8 +2,20 @@
 <div class="row">
 	<hr>
 </div>
-<div class="row ">
+<div class="row">
 	<p>.:: WELCOME MESSAGE ::.</p>
 	<img src="assets/file/welcommessage.jpg" alt="" class="col-sm-12" style="width:100%">
+</div>
+<br>
+<div class="row">
+	<img src="assets/file/welcomemessage2.png" alt="" class="col-sm-12" style="width:100%">
+</div>
+<br>
+<div class="row">
+	<img src="assets/file/welcomemessage3.png" alt="" class="col-sm-12" style="width:100%">
+</div>
+<br>
+<div class="row">
+	<img src="assets/file/welcomemessage4.png" alt="" class="col-sm-12" style="width:100%">
 </div>
 <?php include 'footer.php';?>
