7adda14b74f0a35bb7955e510f5fe2b8844b3352
te chaiwat <te.chaiwat@gmail.com>
2017-07-24 15:40:46 +0700

    update proceeding

--- Proceeding.php
+++ Proceeding.php
@@ -2,12 +2,12 @@
 <div class="col-sm-12">
 	:: >><a href="download.php?filename=CARD2017_Vol1.pdf">  Download File PROCEEDING VOL.1</a><< ::
 	<br>
-	<embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol1.pdf" width="100%" height='1200px;' type='application/pdf'/>
+	<!-- <embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol1.pdf" width="100%" height='1200px;' type='application/pdf'/> -->
 </div>
 <div class="col-sm-12"> <hr> </div>
 <div class="col-sm-12">
 	:: >><a href="download.php?filename=CARD2017_Vol2.pdf">  Download File PROCEEDING VOL.2</a><< ::
 	<br>
-	<embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol2.pdf" width="100%" height='1200px;' type='application/pdf'/>
+	<!-- <embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol2.pdf" width="100%" height='1200px;' type='application/pdf'/> -->
 </div>
 <?php include 'footer.php';?>
