f8d933d94bd9b923518105b04f9ecf182e67c69a
chaiwat <mynameistecs51@gmail.com>
2017-08-08 21:43:52 +0700

    update proceeding

--- Proceeding.php
+++ Proceeding.php
@@ -1,14 +1,14 @@
 <?php include 'header.php';?>
 <div class="col-sm-12 text-center">
-	<h1> .:: รออัพเดทข้อมูล ::.</h1>
-	<!-- :: >><a href="download.php?filename=CARD2017_Vol1.pdf">  Download File PROCEEDING VOL.1</a><< ::
+	<!-- <h1> .:: รออัพเดทข้อมูล ::.</h1> -->
+	<a href="download.php?filename=Proceeding_ECTI-CARD2017_Vol1_final.pdf"> <h2>:: >> Download File PROCEEDING VOL.1 << ::</h2></a>
 	<br>
-	<embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol1.pdf" width="100%" height='1200px;' type='application/pdf'/>
+	<img src="./assets/img/Proceeding_ECTI-CARD2017_Vol1_final.jpg" width="100%" height='1200px;' />
 </div>
 <div class="col-sm-12"> <hr> </div>
 <div class="col-sm-12">
-	:: >><a href="download.php?filename=CARD2017_Vol2.pdf">  Download File PROCEEDING VOL.2</a><< ::
-	<br> -->
-	<!-- <embed src="./assets/file/Proceeding_ECTI-CARD2017_Vol2.pdf" width="100%" height='1200px;' type='application/pdf'/> -->
+	<a href="download.php?filename=Proceeding_ECTI-CARD2017_Vol2_final.pdf"> <h2>:: >>  Download File PROCEEDING VOL.2 << ::</h2></a>
+	<br>
+	<img src="./assets/img/Proceeding_ECTI-CARD2017_Vol2_final.jpg" width="100%" height='1200px;' />
 </div>
 <?php include 'footer.php';?>
